55676dd9cf9dd29c51e48d46f62ed3eb05af843d
wijayahacksload <61936293+wijayahacksload@users.noreply.github.com>
2020-04-14 18:07:51 +0700

    Add files via upload

---
+++ SCT00LSv1.sh
@@ -0,0 +1,558 @@
+#!system/bin/bash
+
+clear
+sleep 3
+echo "\033[1;33m[==========================================]"
+sleep 1
+echo "[           WELCOME IN MY CHANNEL          ]"
+sleep 1
+echo "\033[1;33m[==========================================]"
+sleep 1
+echo "[ Create By : Mr.WijayaHacks_Load404       ]"
+sleep 1
+echo "[ No Wa     : 082131764311                 ]"
+sleep 1
+echo "[ Kalo Mau Recode Ijin Dulu Cokk!!         ]"
+sleep 1
+echo "\033[1;33m[==========================================]"
+sleep 1
+echo ""
+sleep 1
+echo ""
+sleep 2
+echo "\033[1;32mHay gan.."
+sleep 1
+echo "\033[1;32mSelamat Datang"
+sleep 1
+read -p "Silahkan Masukkan Nama Anda Disini:" nama
+sleep 1
+echo "\033[1;32mSelamat Datang Tuan" $nama
+echo "\033[1;32mSelamat Menikmati :)"
+echo ""
+echo ""
+
+sleep 3
+echo "SABAR MASIH LOADING"
+sleep 1
+echo "|####||1%||################################|"
+sleep 1
+echo "|#####||2%||###############################|"
+echo "|######||3%||##############################|"
+sleep 2
+echo "|#######||4%||#############################|"
+sleep 1
+echo "|########||5%||############################|"
+sleep 1
+echo "|#########||6%||###########################|"
+sleep 1
+echo "|##########||7%||##########################|"
+echo "|###########||8%||#########################|"
+echo "|############||9%||########################|"
+echo "|#############||10%||######################|"
+echo "|##############||11%||#####################|"
+echo "|###############||12%||####################|"
+echo "|################||13%||###################|"
+sleep 2
+echo "|#################||14%||##################|"
+echo "|##################||15%||#################|"
+echo "|###################||16%||################|"
+sleep 1
+echo "|####################||17%||###############|"
+sleep 2
+echo "|#####################||18%||##############|"
+sleep 1
+echo "|######################||19%||#############|"
+sleep 1
+echo "|#######################||20%||############|"
+sleep 1
+echo "|########################||21%||###########|"
+echo "|#########################||22%||##########|"
+echo "|##########################||23%||#########|"
+echo "|###########################||24%||########|"
+echo "|############################||25%||#######|"
+echo "|#############################||26%||######|"
+echo "|##############################||27%||#####|"
+echo "|###############################||28%||####|"
+echo "|###############################||29%||####|"
+echo "|##############################||30%||#####|"
+echo "|#############################||31%||######|"
+echo "|############################||32%||#######|"
+echo "|###########################||33%||########|"
+echo "|##########################||34%||#########|"
+sleep 3
+echo "|######################÷##||35%||##########|"
+echo "|########################||37%||###########|"
+echo "|#######################||38%||############|"
+echo "|######################||39%||#############|"
+echo "|#####################||40%||##############|"
+sleep 1
+echo "|####################||41%||###############|"
+sleep 1
+echo "|###################||42%||################|"
+echo "|##################||43%||#################|"
+echo "|#################||44%||##################|"
+echo "|################||45%||###################|"
+echo "|###############||46%||####################|"
+echo "|##############||47%||#####################|"
+echo "|#############||48%||######################|"
+sleep 2
+echo "|############||49%||#######################|"
+sleep 1
+echo "|###########||50%||########################|"
+echo "|##########||51%||#########################|"
+sleep 1
+echo "|#########||52%||##########################|"
+sleep 1
+echo "|########||53%||###########################|"
+echo "|#######||54%||############################|"
+echo "|######||55%||#############################|"
+echo "|#####||56%||##############################|"
+echo "|####||57%||###############################|"
+echo "|####||58%||###############################|"
+echo "|#####||59%||##############################|"
+sleep 2
+echo "|######||60%||#############################|"
+sleep 1
+echo "|#######||61%||############################|"
+sleep 1
+echo "|########||62%||###########################|"
+sleep 1
+echo "|#########||63%||##########################|"
+echo "|##########||64||##########################|"
+echo "|###########||65||#########################|"
+sleep 1
+echo "|############||66%||#######################|"
+sleep 1
+echo "|#############||67%||######################|"
+sleep 2
+echo "|##############||68%||#####################|"
+sleep 1
+echo "|###############||69%||####################|"
+sleep 1
+echo "|################||70%||###################|"
+sleep 1
+echo "|#################||71%||##################|"
+echo "|##################||72%||#################|"
+echo "|###################||73%||################|"
+echo "|####################||74%||###############|"
+echo "|#####################||75%||##############|"
+echo "|######################||76%||#############|"
+echo "|#######################||77%||############|"
+echo "|########################||78%||###########|"
+echo "|#########################||79%||##########|"
+echo "|##########################||80%||#########|"
+echo "|###########################||81%||########|"
+echo "|############################||82%||#######|"
+sleep 1
+echo "|#############################||83%||######|"
+sleep 1
+echo "|##############################||84%||#####|"
+echo "|###############################||85%||####|"
+echo "|###############################||86%||####|"
+echo "|##############################||87%||#####|"
+echo "|#############################||88%||######|"
+echo "|############################||89%||#######|"
+echo "|###########################||90%||########|"
+sleep 1
+echo "|##########################||91%||#########|"
+sleep 1
+echo "|#########################||92%||##########|"
+sleep 1
+echo "|########################||93%||###########|"
+sleep 1
+echo "|#######################||94%||############|"
+echo "|######################||95%||#############|"
+echo "|#####################||96%||##############|"
+echo "|####################||97%||###############|"
+echo "|###################||98%||################|"
+echo "|##################||99%||#################|"
+sleep 2
+echo "|#################||100%||#################|"
+sleep 1
+
+sleep 3
+clear
+
+sleep 1
+echo "\033[0;34m[==========================================]"
+sleep 1
+echo "\033[0;34m         Selamat datang di SCT00LS     "
+sleep 1
+echo "\033[0;34m[==========================================]"
+echo "\033[1;32mCreate By : WijayaHacks_Load404"
+sleep 1
+echo "\033[1;32mNo Wa     : 082131764311"
+sleep 1
+echo "\033[1;33mINGET KALO MAU RECODE IJIN DULU COKK!!"
+sleep 1
+echo "\033[1;33mSilahkan Pilih Tools Yang Kalian Butuhkan"
+sleep 1
+echo "\033[0;34m[==========================================]"
+echo "\033[0;34m                  SCT00LS"
+sleep 1
+echo "\033[0;35m[==========================================]"
+echo "\033[0;35m[1] INSTALL NMAP"
+sleep 1
+echo "\033[1;33m[==========================================]"
+echo "\033[1;33m[2] INSTALL LAZYMUX"
+sleep 1
+echo "\033[1;32m[==========================================]"
+echo "\033[1;32m[3] INSTALL IP-TRACER"
+sleep 1
+echo "\033[0;34m[==========================================]"
+echo "\033[0;34m[4] INSTALL TERMUX-THEME"
+sleep 1
+echo "\033[1;32m[==========================================]"
+echo "\033[1;32m[5] INSTALL SIMPLE SPAM"
+sleep 1
+echo "\033[1;33m[==========================================]"
+echo "\033[1;33m[6] INSTALL VIRTEX"
+sleep 1
+echo "\033[0;35m[==========================================]"
+echo "\033[0;35m[7] INSTALL SPAM-CALL "
+sleep 1
+echo "\033[0;34m[==========================================]"
+echo "\033[0;34m[8] INSTALL DDOS TORSHAMMER"
+sleep 1
+echo "\033[1;33m[==========================================]"
+echo "\033[1;33m[9] INSTALL DarkPremium"
+sleep 1
+echo "\033[1;32m[==========================================]"
+echo "\033[1;32m[10] INSTALL TOOLS HACK WIFI"
+sleep 1
+echo "\033[1;33m[==========================================]"
+echo "\033[1;33m[11] INSTALL SQLMAP"
+sleep 1
+echo "\033[0;34m[==========================================]"
+echo "\033[0;34m[12] JOIN GRUP WHATSAPP"
+sleep 1
+echo "\033[0;35m[==========================================]"
+echo "\033[0;35m[13] INSTALL FACEBOOK VIA TERMUX"
+sleep 1
+echo "\033[1;33m[==========================================]"
+echo "\033[1;33m[14] INSTALL WIFI HACKER"
+sleep 1
+echo "\033[1;32m[==========================================]"
+echo "\033[1;32m[15] INSTALL "
+sleep 1
+echo "\033[0;34m[==========================================]"
+echo "\033[0;34m[16] INSTALL "
+sleep 1
+echo "\033[1;32m[==========================================]"
+echo "\033[1;32m[17] INSTALL "
+sleep 1
+echo "\033[1;33m[==========================================]"
+echo "\033[1;33m[18] INSTALL "
+sleep 1
+echo "\033[1;33m[==========================================]"
+echo "\033[1;33m[19] INSTALL "
+sleep 1
+echo "\033[0;35m[==========================================]"
+echo "\033[0;35m[20] INSTALL "
+sleep 1
+echo "\033[1;37m[==========================================]"
+echo "\033[1;37m[99] EXIT"
+sleep 1
+echo "\033[1;37m[==========================================]"
+read -p "MASUKKAN PILIHAN ANDA :" pilih
+if [ $pilih = "1" ]
+then
+	clear
+	echo "Installing NMAP..."
+	sleep 3
+	cd $HOME
+	pkg update
+	pkg upgrade
+	pkg install nmap
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "2" ]
+then
+	clear
+	echo "Installing LAZYMUX..."
+	sleep 3
+	cd $HOME
+	pkg update
+	pkg upgrade
+	apt install python2
+	apt python2-dev
+	apt install git
+	git clone https://github.com/Gameye98/lazymux.git
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "3" ]
+then
+	clear
+	echo "Installing IP-TRACER..."
+	sleep 3
+	cd $HOME
+	pkg update
+	upgrade
+	git clone https://github.com/Rajkumrdusad/IP-Tracer.git
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "4" ]
+then
+	clear
+	echo "Installing TERMUX-THEME..."
+	sleep 3
+	cd $HOM
+	pkg update
+	pkg upgrade
+	pkg install python2
+	pkg install git
+	git clone https://github.com/saydog/termux-theme
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "5" ]
+then
+	clear
+	echo "Installing SIMPLE-SPAM..."
+	sleep 3
+	cd $HOME
+	pkg update
+	pkg upgrade
+	pkg install git
+	pkg install php
+	git clone https://github.com/dz-id/SimpleSpamOtp
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "6" ]
+then
+	clear
+	echo "Installing VIRTEX "
+	sleep 3
+	cd $HOME
+	pkg update
+	pkg upgrade
+	pkg install git
+	pkg install curl
+	pkg install figlet
+	pkg install ruby
+	gem install lolcat
+	git clone https://github.com/muhammadfathul/VIRTEX
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "7" ]
+then
+	clear
+	echo "INSTALLING SPAM-CALL"
+	sleep 3
+	cd $HOME
+	pkg update
+	pkg upgrade
+	pkg install git && pkg install php
+	git clone https://github.com/storiku/Call
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "8" ]
+then
+	clear
+	echo "INSTALLING DDOS TORSHAMMER"
+	sleep 3
+	cd $HOME
+	pkg update
+	pkg upgrade
+	apt install tor
+	apt install python2
+	apt install git
+	git clone https://github.com/dotfighter/torshammer.git
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "9" ]
+then
+	clear
+	echo "INSTALLING DarkPremium"
+	sleep 3
+	cd $HOME
+	pkg update 
+	pkg upgrade
+	pkg install python2
+	pkg install git
+	git clone https://github.com/TheMagizz/DarkPremium
+	cd DarkPremium
+	pip2 install -r requirements.txt
+	cd
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "10" ]
+then
+	clear
+	echo "INSTALLING WIFI TOOLS"
+	sleep 3
+	cd $HOME
+	apt update
+	apt upgrade
+	apt install git
+	apt install wget
+	apt install proot
+	pkg install curl
+	pkg install clang
+	pkg install tsudo
+	pkg install tsu
+	git clone https://github.com/kumpulanremaja/wifi
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "11" ]
+then
+	clear
+	echo "INSTALLING SQLMAP"
+	sleep 3
+	cd $HOME
+	apt update
+	apt upgrade
+	apt install git
+	apt install python2
+	git clone https://github.com/sqlmapproject/sqlmap
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "12" ]
+then
+	clear
+	echo "\033[1;33m[==========================================]"
+	echo "\033[0;32m[~~~~~~~~~●CyberOneCrimeIndonesia●~~~~~~~~]"
+	echo "\033[1;33m[==========================================]"
+	echo "\033[0;32m[https://chat.whatsapp.com/FdvpYo8OSZw9dERuxqHQuS]
+	echo "\033[1;33m[==========================================]"
+	sleep 15
+fi
+if [ $pilih = "13" ]
+then
+	clear
+	echo "INSTALLING FACEBOOK TERMUX"
+	sleep 3
+	cd $HOME
+	apt update
+	apt upgrade
+	pkg install python2
+	pkg install git
+	pip2 install mechanize
+	git clone https://github.com/pashayogi/SETAN
+	echo "Penginstallan Selesai"		
+	sleep 3
+fi
+if [ $pilih = "14" ]
+then
+	clear
+	echo "Installing WifiHacker"
+	sleep 3
+	cd $HOME
+	pkg update
+	pkg upgrade
+	pkg install git
+	git clone https://github.com/esc0rtd3w/wifi-hacker
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "15" ]
+then
+	clear
+	echo "INSTALLING "
+	sleep 3
+	cd $HOME
+	
+	
+	
+	
+	
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "16" ]
+then
+	clear
+	echo "INSTALLING "
+	sleep 3
+	cd $HOME
+	
+	
+	
+	
+	
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "17" ]
+then
+	clear
+	echo "INSTALLING "
+	sleep 3
+	cd $HOME
+	
+	
+	
+	
+	
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "18" ]
+then
+	clear
+	echo "INSTALLING "
+	sleep 3
+	cd $HOME
+	
+	
+	
+	
+	
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "19" ]
+then
+	clear
+	echo "INSTALLING "
+	sleep 3
+	cd $HOME
+	
+	
+	
+	
+	
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "20" ]
+then
+	clear
+	echo "INSTALLING "
+	sleep 3
+	cd $HOME
+	
+	
+	
+	
+	
+	echo "Penginstallan Selesai"
+	sleep 3
+fi
+if [ $pilih = "99" ]
+then
+	clear
+	sleep 2
+	cd $HOME
+	echo = "exit"
+	echo = "Terimakasih Telah Menggunakan ScT00ls"
+	echo = "____________________________________________"
+	sleep 3
+fi
+else
+	clear
+	echo = "Di Pilihannya Ga Ada Cok"
+	echo = "Teliti Lagi!!"
+	sleep 2
+	echo
+fi
